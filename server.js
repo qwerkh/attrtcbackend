1af537b70c10594f8a4923fb2a4b6b44153f3ee9
qwerkh <narongsao98@gmail.com>
2026-01-13 14:26:52 +0700

    d

--- server.js
+++ server.js
@@ -18,12 +18,13 @@ app.use(express.json());
 app.use(cors({
     origin: [
         '*',
+        'https://rtcbtb-a447c.web.app/',
         'http://localhost:8080',
         'http://127.0.0.1:8080',
         'http://10.11.0.45:8080' // your PC IP for mobile testing
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization','token'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'token'],
 }));
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
