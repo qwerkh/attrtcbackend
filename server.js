1dcf9278cee736916be1fdb16a881470c8af820b
qwerkh <narongsao98@gmail.com>
2026-01-13 16:03:43 +0700

    d

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ connectDB();
 import employeeRoute from "./routes/employeeRoute.js";
 import authRoute from "./routes/authRoute.js";
 import checkInRoute from "./routes/checkInRoute.js";
-app.use(cors({
+app.use(cors());
+/*app.use(cors({
     origin: [
         'https://rtcbtb-a447c.web.app',
         'http://localhost:8080',
@@ -22,7 +23,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'token'],
     credentials: true
-}));
+}));*/
 
 app.use((req, res, next) => {
     if (req.method === "OPTIONS") {
